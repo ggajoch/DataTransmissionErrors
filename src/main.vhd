e03e1530fdcd62f1d1957688536e098c9218508d
Grzegorz Gajoch <gajoch@gmail.com>
2014-12-07 18:15:45 +0100

    Added instantiation of number controller, test with output clock with regulated frequency with keyboard and digits.

--- src/main.vhd
+++ src/main.vhd
@@ -12,6 +12,7 @@ entity main is
 		buttonDown : in std_logic;
 		switches : in std_logic_vector(7 downto 0);
 		LED : in std_logic_vector(7 downto 0);
+		out_clock : out std_logic;
 		clock_100MHz : in std_logic
 	);
 end entity main;
@@ -19,6 +20,7 @@ end entity main;
 architecture RTL of main is
 signal clock_1MHz : std_logic;
 signal clock_1kHz : std_logic;
+signal clock_10Hz : std_logic;
 signal clock_1Hz : std_logic;
 signal actual_string : string(8 downto 1) := "12345678";
 signal actual_dots : std_logic_vector(8 downto 1) := "00000000";
@@ -30,47 +32,27 @@ signal buttonRightDebounced : std_logic;
 signal buttonUpDebounced : std_logic;
 signal buttonDownDebounced : std_logic;
 
-signal one:std_logic_vector(6 downto 0) := "0000000";
-
+signal prescaler_value : integer range 0 to 10**8 := 1;
 begin
 	
-	actual_dots <= to_stdlogicvector(to_bitvector(one & clock_1Hz) sll changing_nr);
-	
-	
-	process(clock_1kHz) is
-		variable last_Left : std_logic := '0';
-		variable last_Right : std_logic := '0';
-		variable last_Up : std_logic := '0';
-		variable last_Down : std_logic := '0';
-	begin
-		if( rising_edge(clock_1kHz) ) then
-			if( last_Left = '1' and buttonLeftDebounced = '0') then
-				if( changing_nr < 7 ) then
-					changing_nr <= changing_nr+1;
-				end if;
-			end if;
-			
-			if( last_Right = '1' and buttonRightDebounced = '0') then
-				if( changing_nr > 0 ) then
-					changing_nr <= changing_nr-1;
-				end if;
-			end if;
-			
-			if( last_Up = '1' and buttonUpDebounced = '0') then
-                actual_string(changing_nr+1) <= character'val(character'pos(actual_string(changing_nr+1)) + 1);
-            end if;
-			
-			if( last_Down = '1' and buttonDownDebounced = '0') then
-                actual_string(changing_nr+1) <= character'val(character'pos(actual_string(changing_nr+1)) - 1);
-            end if;
-                        
-			last_Left := buttonLeftDebounced;
-			last_Right := buttonRightDebounced;
-			last_Up := buttonUpDebounced;
-			last_Down := buttonDownDebounced;
-		end if;
-	end process ;
-	
+	numController_inst : entity work.numberController
+		generic map(nr_of_digits => 8)
+		port map(displayString  => actual_string,
+			     displayDots    => actual_dots,
+			     integer_value  => prescaler_value,
+			     buttonLeft     => buttonLeftDebounced,
+			     buttonRight    => buttonRightDebounced,
+			     buttonUp       => buttonUpDebounced,
+			     buttonDown     => buttonDownDebounced,
+			     keyboard_clock => clock_1kHz,
+			     dot_clock      => clock_10Hz);
+
+	prescalerTestControlled : entity work.prescaler
+       port map(clk_input => clock_100MHz,
+                clk_output => out_clock,
+                reset => '0',
+                presc => prescaler_value);
+
 	SevenSegControl_inst : entity work.SevenSegControl
 		port map(input => actual_string,
 				 input_dots => actual_dots,
@@ -117,6 +99,11 @@ begin
                   clk_output => clock_1kHz,
                   reset => '0',
                   presc => 1000);
+	prescaler10 : entity work.prescaler
+           port map(clk_input => clock_1kHz,
+                    clk_output => clock_10Hz,
+                    reset => '0',
+                    presc => 100);
     prescaler1 : entity work.prescaler
            port map(clk_input => clock_1kHz,
                     clk_output => clock_1Hz,
