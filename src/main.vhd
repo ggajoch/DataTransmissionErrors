d048dedbabf4d70280899386ea6e560916002294
Grzegorz Gajoch <gajoch@gmail.com>
2014-12-09 03:15:22 +0100

    Basic screen controller based on state machine - untested.

--- src/main.vhd
+++ src/main.vhd
@@ -37,15 +37,70 @@ signal uart_data : std_logic_vector(7 downto 0) := "00000000";
 signal uart_TC : std_logic := '0';
 
 
+signal speed_string : string(8 downto 1) := "12345678";
+signal speed_dots : std_logic_vector(8 downto 1) := "00000000";
+
 signal actual_string : string(8 downto 1) := "12345678";
 signal actual_dots : std_logic_vector(8 downto 1) := "00000000";
 
+type displayStates is (Speed, Protocol, WaitTicks, Wait1sec, WelcomeSpeed, WelcomeProtocol, Welcome);
+
 begin
+	
+	displayStateMaching : process(clock_1kHz) is
+		variable State : displayStates := Welcome;
+		variable StateAfterWait : displayStates;
+		variable ticks_left : integer := 0;
+		variable last_change_button : std_logic := '1';
+		variable middle_pressed : boolean := FALSE;
+	begin
+		if( rising_edge(clock_1kHz) ) then
+			middle_pressed := ( last_change_button = '0' and buttonMiddleDebounced = '1' );
+			actual_dots <= (others => '0'); 
+			case State is
+				when Speed =>
+					actual_dots <= speed_dots;
+					actual_string <= speed_string;
+					if( middle_pressed ) then
+						StateAfterWait := WelcomeProtocol;
+						State := Wait1sec;
+					end if;
+				when Protocol =>
+					actual_string <= "prot-val";
+					if( middle_pressed ) then
+						StateAfterWait := WelcomeSpeed;
+						State := Wait1sec;
+					end if;
+				when WelcomeSpeed =>
+					actual_string <= "-speed--";
+					StateAfterWait := Speed;
+					State := Wait1sec;
+				when WelcomeProtocol =>
+					actual_string <= "--prot--";
+					StateAfterWait := Protocol;
+					State := Wait1sec;
+				when Welcome =>
+					actual_string <= "-hello--";
+					StateAfterWait := WelcomeSpeed;
+					State := Wait1sec;
+				when Wait1sec => 
+					ticks_left := 999;
+					State := WaitTicks;
+				when WaitTicks =>
+					if( ticks_left > 0 ) then
+						ticks_left := ticks_left-1;
+					else
+						State := StateAfterWait;
+					end if;
+			end case;
+			last_change_button := buttonMiddleDebounced;
+		end if;
+	end process displayStateMaching;
 	
 	numController_inst : entity work.numberController
 		generic map(nr_of_digits => 8)
-		port map(displayString  => actual_string,
-			     displayDots    => actual_dots,
+		port map(displayString  => speed_string,
+			     displayDots    => speed_dots,
 			     integer_value  => prescaler_value,
 			     buttonLeft     => buttonLeftDebounced,
 			     buttonRight    => buttonRightDebounced,
