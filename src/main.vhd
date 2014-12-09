d9f7a534175e0c715c11043ac49e9f57411d734a
Grzegorz Gajoch <gajoch@gmail.com>
2014-12-09 02:35:42 +0100

    example to use uart with changing baudrate - to be tested.

--- src/main.vhd
+++ src/main.vhd
@@ -1,5 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity main is
 	port (
@@ -11,7 +12,7 @@ entity main is
 		buttonUp : in std_logic;
 		buttonDown : in std_logic;
 		switches : in std_logic_vector(7 downto 0);
-		LED : in std_logic_vector(7 downto 0);
+		LED : out std_logic_vector(7 downto 0);
 		out_clock : out std_logic;
 		clock_100MHz : in std_logic
 	);
@@ -22,8 +23,6 @@ signal clock_1MHz : std_logic;
 signal clock_1kHz : std_logic;
 signal clock_10Hz : std_logic;
 signal clock_1Hz : std_logic;
-signal actual_string : string(8 downto 1) := "12345678";
-signal actual_dots : std_logic_vector(8 downto 1) := "00000000";
 
 signal buttonMiddleDebounced : std_logic;
 signal buttonLeftDebounced : std_logic;
@@ -32,6 +31,15 @@ signal buttonUpDebounced : std_logic;
 signal buttonDownDebounced : std_logic;
 
 signal prescaler_value : integer range 0 to 10**8 := 1;
+signal clock_prescaled : std_logic;
+
+signal uart_data : std_logic_vector(7 downto 0) := "00000000";
+signal uart_TC : std_logic := '0';
+
+
+signal actual_string : string(8 downto 1) := "12345678";
+signal actual_dots : std_logic_vector(8 downto 1) := "00000000";
+
 begin
 	
 	numController_inst : entity work.numberController
@@ -46,11 +54,14 @@ begin
 			     keyboard_clock => clock_1kHz,
 			     dot_clock      => clock_10Hz);
 
+	out_clock <= clock_prescaled;
 	prescalerTestControlled : entity work.prescaler
        port map(clk_input => clock_100MHz,
-                clk_output => out_clock,
+                clk_output => clock_prescaled,
                 reset => '0',
                 presc => prescaler_value);
+
+
 
 	SevenSegControl_inst : entity work.SevenSegControl
 		port map(input => actual_string,
@@ -58,6 +69,25 @@ begin
 			     digits => digits,
 			     segments => segments,
 			     segment_change_clock => clock_1kHz);
+
+
+--------------- UART ------------------------------------
+
+	uart_inst : entity work.TxTest
+		port map(TxPin    => LED(0),
+			     TxClock  => clock_prescaled,
+			     Data     => uart_data,
+			     DataFlag => clock_10Hz,
+			     TC       => uart_TC);
+
+	uart_proc : process(clock_10Hz) is
+	begin
+		if( rising_edge(clock_10Hz) ) then
+			uart_data <= std_logic_vector(unsigned(uart_data)+1);
+		end if;
+	end process uart_proc;
+		
+--------------- DEBOUNCING ------------------------------------
 			     
 	debouncerButtonMiddle : entity work.debouncer
 		generic map(TicksBetweenEdges => 10)
@@ -87,6 +117,8 @@ begin
           port map(input  => buttonDown,
                    output => buttonDownDebounced,
                    clock  => clock_1kHz);
+
+--------------- PRESCALERS ------------------------------------
                    
     prescaler1M : entity work.prescaler
         port map(clk_input => clock_100MHz,
