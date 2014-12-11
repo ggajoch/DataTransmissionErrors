824416a2237aa6fea98eb660f6ef7a265cc3cc00
Grzegorz Gajoch <gajoch@gmail.com>
2014-12-11 01:27:40 +0100

    basic implementation of structure of reconfigurable PLL on artix-7 fpga

---
+++ src/PLL/PLLValues.vhd
@@ -0,0 +1,105 @@
+-- generation of commands to be written to PLL to set clock
+-- ref: http://www.xilinx.com/support/documentation/ip_documentation/clk_wiz/v5_1/pg065-clk-wiz.pdf
+
+library ieee;
+use ieee.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity PLLValues is
+	port (
+		DIVCLK_DIVIDE : integer range 0 to 255;
+		CLKFBOUT_MULT : integer range 0 to 255;
+		CLKFBOUT_FRAC : integer range 0 to 1023;
+		CLKFBOUT_FRAC_EN : std_logic;
+		
+		data_valid : std_logic;
+		
+		response : out std_logic_vector(1 downto 0);
+		done : out std_logic;
+		clock_100MHz : in std_logic;
+		
+
+		PLL_clock_out : out std_logic;
+		PLL_locked : out std_logic
+	);
+end entity PLLValues;
+
+architecture RTL of PLLValues is
+	signal C_BASEADDR : std_logic_vector(10 downto 0) := "00000000000";
+		
+	signal clock_output : std_logic;
+	signal locked : std_logic;
+	signal write_address : std_logic_vector(10 downto 0);
+	signal write_data : std_logic_vector(31 downto 0);
+	signal write_valid : std_logic;
+	signal axi4_write_response : std_logic_vector(1 downto 0); -- 00 - OK
+	signal commmand_done : std_logic;
+	
+	type FSMStates_t is (Idle, Valid1, Valid2, Write, WaitF);
+	
+begin
+	
+	PLL_clock_out <= clock_output;
+	PLL_locked <= locked;
+	axi4masterPLL_inst : entity work.axi4masterPLL
+		port map(clock_100MHz        => clock_100MHz,
+			     clock_output        => clock_output,
+			     locked              => locked,
+			     write_address       => write_address,
+			     write_data          => write_data,
+			     write_valid         => write_valid,
+			     axi4_write_response => axi4_write_response,
+			     commmand_done       => commmand_done);
+	
+	mainProc : process(clock_100MHz) is
+		variable State : FSMStates_t := Idle;
+		variable NextState : FSMStates_t := Idle;
+		variable last_data_valid : std_logic := '0';
+		variable address_Reg0 : std_logic_vector(10 downto 0) := "01000000000"; --0x200
+		variable address_Reg23 : std_logic_vector(10 downto 0) := "01001011100"; --0x25C
+	begin
+		if( rising_edge(clock_100MHz) ) then
+			case State is 
+				when Idle =>
+					write_valid <= '0';
+					response <= "00";
+					done <= '1';
+				
+					if( last_data_valid = '0' and data_valid = '1' ) then
+						State := Write;
+						NextState := Valid1;
+						
+						write_address <= std_logic_vector(unsigned(C_BASEADDR) + unsigned(address_Reg0));
+						write_data <= "00000" & CLKFBOUT_FRAC_EN & 
+									  std_logic_vector(to_unsigned(CLKFBOUT_FRAC, 10)) & 
+									  std_logic_vector(to_unsigned(CLKFBOUT_MULT, 8)) & 
+									  std_logic_vector(to_unsigned(DIVCLK_DIVIDE, 8));
+					end if;
+				
+				when Valid1 =>
+					write_address <= std_logic_vector(unsigned(C_BASEADDR) + unsigned(address_Reg23));
+					write_data <= "00000000000000000000000000000111";
+					State := Write;
+					NextState := Valid2;
+				when Valid2 =>
+					write_address <= std_logic_vector(unsigned(C_BASEADDR) + unsigned(address_Reg23));
+					write_data <= "00000000000000000000000000000010";
+					State := Write;
+					NextState := Valid2;
+					
+				when Write =>
+					write_valid <= '1';
+					State := WaitF;
+				when WaitF =>
+					write_valid <= '0';
+					if( commmand_done = '1' ) then
+						response <= axi4_write_response;
+						State := NextState;
+					end if;
+			end case;
+			
+			last_data_valid := data_valid;
+		end if;
+	end process mainProc;
+	
+end architecture RTL;
---
+++ src/PLL/axi4-master.vhd
@@ -0,0 +1,115 @@
+-- basic implementation of AXI4-Lite master module
+-- ref: http://www.xilinx.com/support/documentation/ip_documentation/clk_wiz/v5_1/pg065-clk-wiz.pdf
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity axi4masterPLL is
+	port (
+		clock_100MHz : in std_logic;
+		clock_output : out std_logic;
+		locked : out std_logic;
+		write_address : in std_logic_vector(10 downto 0);
+		write_data : in std_logic_vector(31 downto 0);
+		write_valid : in std_logic;
+		axi4_write_response : out std_logic_vector(1 downto 0); -- 00 - OK
+		commmand_done : out std_logic
+	);
+end entity axi4masterPLL;
+
+architecture RTL of axi4masterPLL is
+	------------------------------------------------
+	---------- AXI4-LITE SIGNALS -------------------
+	------------------------------------------------
+	
+	signal s_axi_aclk : std_logic := '0';
+	signal s_axis_aclk : std_logic := '0';
+	signal s_axi_aresetn : std_logic := '0';
+	-------------------- WRITE ---------------------
+	signal s_axi_awaddr : std_logic_vector(10 downto 0) := (others => '0'); -- address to be written
+	signal s_axi_awvalid : std_logic := '0'; -- valid address
+	signal s_axi_awready : std_logic := '0'; -- IN address ready to be written
+	signal s_axi_wdata : std_logic_vector(31 downto 0) := (others => '0'); -- data to be written
+	signal s_axi_wstb : std_logic_vector(3 downto 0) := (others => '0'); -- strobes for bytes
+	signal s_axi_wvalid : std_logic := '0'; -- valid data
+	signal s_axi_wready : std_logic := '0'; -- IN data ready to be written
+	signal s_axi_bresp : std_logic_vector(1 downto 0) := (others => '0'); -- IN write response
+	signal s_axi_bvalid : std_logic := '0'; -- IN valid response -> written!
+	signal s_axi_bready : std_logic := '0'; -- ready to read response
+	-------------------- READ ---------------------
+	signal s_axi_araddr : std_logic_vector(10 downto 0) := (others => '0');
+	signal s_axi_arvalid : std_logic := '0';
+	signal s_axi_arready : std_logic := '0';
+	signal s_axi_rdata : std_logic_vector(31 downto 0) := (others => '0');
+	signal s_axi_rresp : std_logic_vector(1 downto 0) := (others => '0');
+	signal s_axi_rvalid : std_logic := '0';
+	signal s_axi_rready : std_logic := '0';
+	
+	------------------------------------------------
+	-------------------- MISC ----------------------
+	------------------------------------------------
+	
+	type ProcessAXI4State_t is (Idle, addressWait, addressValid, dataWait, dataValid, waitResponse);
+	
+begin
+	
+	------------------------------------------------
+	---------------- PLL INSTANCE ------------------
+	------------------------------------------------
+		
+	s_axi_aclk <= clock_100MHz;
+	s_axis_aclk <= clock_100MHz;
+	s_axi_aresetn <= '1';
+	
+	------------------------------------------------
+	---------------- MAIN PROCESS ------------------
+	------------------------------------------------
+	
+	mainProc : process(clock_100MHz) is
+		variable last_write_valid : std_logic := '0';
+		variable State : ProcessAXI4State_t := Idle;
+	begin
+		if( rising_edge(clock_100MHz) ) then
+			commmand_done <= '0';
+			case State is 
+				when Idle =>
+					if( last_write_valid = '0' and write_valid = '1' ) then
+						s_axi_awaddr <= write_address;
+						s_axi_wdata <= write_data;
+						s_axi_wstb <= "1111";
+						State := addressWait;
+						
+						s_axi_bready <= '0';
+						s_axi_awvalid <= '0';
+						s_axi_wvalid <= '0';
+						
+						commmand_done <= '1';
+					end if;
+				when addressWait =>
+					if ( s_axi_awready = '1' ) then
+						State := addressValid;
+					end if;
+				when addressValid =>
+					s_axi_awvalid <= '1';
+					State := dataWait;
+				when dataWait =>
+					if ( s_axi_wready = '1' ) then
+						State := dataValid;
+					end if;
+				when dataValid =>
+					s_axi_awvalid <= '0';
+					s_axi_wvalid <= '1';
+					s_axi_bready <= '1';				
+				when waitResponse =>
+					if ( s_axi_bvalid = '1' ) then
+						s_axi_wvalid <= '0';
+						axi4_write_response <= s_axi_bresp;
+						State := Idle;
+					end if;
+			end case;
+			
+			last_write_valid := write_valid;
+		end if;
+	end process mainProc;
+		
+end architecture RTL;
