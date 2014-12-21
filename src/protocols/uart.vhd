768c2d75b99d633e88e0616b14ddb8178168fef5
Grzegorz Gajoch <gajoch@gmail.com>
2014-12-21 21:11:50 +0100

    uart implementation with aligning clock

--- src/protocols/uart.vhd
+++ src/protocols/uart.vhd
@@ -48,67 +48,60 @@ end;
 
 -------------------------- RX -------------------------------------
 
-
-LIBRARY IEEE;
-USE IEEE.STD_LOGIC_1164.ALL;
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity UART_Rx is
 	port( RxPin : in std_logic;
-			data : out std_logic_vector(0 to 7);
-			DataFlag : out std_logic;
-			DataFlagRst : in std_logic;
-			clk : in std_logic;
-			dbg_wire : out std_logic);
+		  fast_clock : in std_logic;
+		  sampling_clock : in std_logic;
+		  sampling_clock_reset : out std_logic;
+		  Data : out std_logic_vector(7 downto 0);
+		  DataFlag : out std_logic;
+		  TransmissionError : out std_logic
+		);
 end UART_Rx;
-architecture RxTest_arch of UART_Rx is
-type State_t is (Idle, StartBit, Capture, NearChange, StopBit);
-signal State : State_t;
-signal StateBegin : State_t;
-signal RxClock : std_logic;
-signal RstCnt : std_logic := '0';
 
-signal dbg_wire_state : std_logic := '0';
+architecture RTL of UART_Rx is
+	type State_t is (Idle, ResetOff, WaitForDataFlag);
+	signal State_SamplingOut : State_t;
+	signal DataFlag_sig : std_logic;
 begin
 	
-	presc0 : entity work.prescaler(prescaler_arch)
-		port map(clk, RxClock, RstCnt, 25);--27 > 2604);
-	
-	process(DataFlagRst, RxPin, RxClock) is
-	variable BitsLeft : natural := 0;
+	process(fast_clock) is
+		variable last_in : std_logic;
+		variable State : State_t;
 	begin
-		if( DataFlagRst = '1' ) then 
-			DataFlag <= '0';
-		elsif( RxPin = '0' and State = Idle ) then
-			RstCnt <= '0';
-			State <= StartBit;
-		elsif( rising_edge(RxClock) ) then
-			case State is
+		if( rising_edge(fast_clock) ) then
+			
+			sampling_clock_reset <= '0';
+			case State is 
 				when Idle =>
-					RstCnt <= '1';
-				when StartBit =>
-					--DataFlag <= '0';
-					RstCnt <= '0';
-					State <= NearChange;
-					BitsLeft := 8;
-				when Capture =>
-					if( BitsLeft > 0 ) then
-						dbg_wire <= dbg_wire_state;
-						dbg_wire_state <= not dbg_wire_state;
-						data(BitsLeft-1) <= RxPin;
-						BitsLeft := BitsLeft-1;
-						State <= NearChange;
-					else
-						State <= StopBit;
+					if( last_in = '1' and RxPin = '0' ) then
+						sampling_clock_reset <= '1';
+						State := ResetOff;
 					end if;
-				when NearChange =>
-					State <= Capture;
-				when StopBit =>
-					State <= Idle;
-					DataFlag <= '1';
-				when others =>
-					State <= Idle;
+				when ResetOff =>
+					sampling_clock_reset <= '0';
+					State := WaitForDataFlag;
+				when WaitForDataFlag =>
+					if( DataFlag_sig = '1' ) then
+						State := Idle;
+					end if;
 			end case;
+			last_in := RxPin;
+			State_SamplingOut <= State;
 		end if;
-	end process;
-end architecture;
+	end process ;
+	
+	
+	DataFlag <= DataFlag_sig;
+	
+	usart_recv: entity work.USART_Rx
+		port map(RxPin             => RxPin,
+			     RxSynchPin        => sampling_clock,
+			     Data              => Data,
+			     DataFlag          => DataFlag_sig,
+			     TransmissionError => TransmissionError);
+end;
