ed76275e9447fe4b123df335570cc85bee8914e1
Grzegorz Gajoch <gajoch@gmail.com>
2014-12-22 04:27:27 +0100

    moved main logic to external file

---
+++ src/Transmitter.vhd
@@ -0,0 +1,147 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity Tx_MAIN is
+	port (
+		digits : out std_logic_vector(7 downto 0);
+		segments : out std_logic_vector(7 downto 0);
+		buttonMiddle : in std_logic;
+		buttonLeft : in std_logic;
+		buttonRight : in std_logic;
+		buttonUp : in std_logic;
+		buttonDown : in std_logic;
+		switchesRaw : in std_logic_vector(15 downto 0);
+		LED : out std_logic_vector(15 downto 0);
+		
+		
+		
+		clock_100MHz : in std_logic;
+		clock_1MHz : in std_logic;
+		clock_1kHz : in std_logic;
+		clock_10Hz : in std_logic;
+		clock_1Hz : in std_logic;
+		
+		
+		
+		output_data : out std_logic;
+		output_clock : out std_logic;
+		output3 : out std_logic
+	);
+end entity Tx_MAIN;
+
+architecture RTL of Tx_MAIN is
+	
+	--------------- CLOCKS ------------------------------------
+	signal clock_prescaled : std_logic;
+	signal data_trigger : std_logic;
+	
+	
+	--------------- USER INPUT --------------------------------
+	
+	signal protocol_sel : integer range 0 to 99;
+	signal speed_integer : integer range 0 to 99;
+	signal speed_exponent : integer range 0 to 9;
+	
+	--------------- COMMON ------------------------------------
+	signal data_out : std_logic_vector(7 downto 0);
+	
+	
+	--------------- UART --------------------------------------
+	signal uart_out : std_logic;
+	signal uart_trigger : std_logic;
+	signal uart_TC : std_logic;
+	
+	--------------- USART -------------------------------------
+	signal usart_out_data : std_logic;
+	signal usart_out_clock : std_logic;
+	signal usart_trigger : std_logic;
+	signal usart_TC : std_logic;
+	
+
+begin
+	
+	--------------- DISPLAY CONTROL ---------------------------
+	
+	display_inst : entity work.displayController
+		port map(buttonMiddle                => buttonMiddle,
+			     buttonLeft                  => buttonLeft,
+			     buttonRight                 => buttonRight,
+			     buttonUp                    => buttonUp,
+			     buttonDown                  => buttonDown,
+			     clock_dot                   => clock_10Hz,
+			     clock_keyboard              => clock_1kHz,
+			     protocol_sel_out            => protocol_sel,
+			     sci_controller_integer_out  => speed_integer,
+			     sci_controller_exponent_out => speed_exponent,
+			     digits 			 		 => digits,
+			     segments				 	 => segments,
+			     segment_mux_clock			 => clock_1kHz);
+
+	
+
+	--------------- CLOCK GENERATION --------------------------
+
+	clock_inst : entity work.clockController
+		port map(speed_integer => speed_integer,
+			     speed_exp     => speed_exponent,
+			     clock_100MHz  => clock_100MHz,
+			     clock_out     => clock_prescaled,
+			     reset_presc   => '0');
+
+	trigger_gen_inst : entity work.prescaler
+		generic map(max_presc => 20)
+		port map(clk_input  => clock_prescaled,
+			     clk_output => data_trigger,
+			     reset      => '0',
+			     presc      => 20);
+
+
+	--------------- DATA GENERATION ---------------------------
+	
+	data_out <= switchesRaw(7 downto 0);
+	
+	
+	--------------- USER INPUTS -------------------------------
+	
+	process(protocol_sel) is
+	begin
+		case protocol_sel is
+			when 0 =>
+				output_data <= uart_out;
+				output_clock <= '0';
+			when 1 =>
+				output_data <= usart_out_data;
+				output_clock <= usart_out_clock;
+			when others =>
+				output_data <= '0';
+				output_clock <= '0';
+		end case;
+	end process;
+	
+	
+	--------------- UART --------------------------------------
+
+	uart_trigger <= data_trigger;
+	
+	uart_inst : entity work.UART_Tx	
+		port map(TxPin    => uart_out,
+			     TxClock  => clock_prescaled,
+			     Data     => data_out,
+			     DataFlag => uart_trigger,
+			     TC       => uart_TC);
+
+	--------------- USART -------------------------------------
+	
+	usart_trigger <= data_trigger;
+	usartTX_inst : entity work.USART_Tx
+		port map(TxPin      => usart_out_data,
+			     TxSynchPin => usart_out_clock,
+			     TxClock    => clock_prescaled,
+			     Data       => data_out,
+			     DataFlag   => usart_trigger,
+			     TC         => usart_TC); 
+
+
+end architecture RTL;
+
--- src/main.vhd
+++ src/main.vhd
@@ -13,6 +13,8 @@ entity main is
 		buttonDownRaw : in std_logic;
 		switchesRaw : in std_logic_vector(15 downto 0);
 		LED : out std_logic_vector(15 downto 0);
+		
+		
 		out_clock : out std_logic;
 		uart_out : out std_logic;
 		
@@ -24,129 +26,42 @@ entity main is
 		clock_100MHz : in std_logic
 	);
 end entity main;
+	
+	architecture RTL of main is
+	signal clock_1MHz : std_logic;
+	signal clock_1kHz : std_logic;
+	signal clock_10Hz : std_logic;
+	signal clock_1Hz : std_logic;
+	
+	signal buttonMiddle : std_logic;
+	signal buttonLeft : std_logic;
+	signal buttonRight : std_logic;
+	signal buttonUp : std_logic;
+	signal buttonDown : std_logic;
 
-architecture RTL of main is
-signal clock_1MHz : std_logic;
-signal clock_1kHz : std_logic;
-signal clock_10Hz : std_logic;
-signal clock_1Hz : std_logic;
-
-signal buttonMiddle : std_logic;
-signal buttonLeft : std_logic;
-signal buttonRight : std_logic;
-signal buttonUp : std_logic;
-signal buttonDown : std_logic;
-
-signal prescaler_value : integer range 0 to 10**8 := 1;
-signal clock_prescaled : std_logic;
-
-signal uart_data : std_logic_vector(7 downto 0) := "00000000";
-signal uart_TC : std_logic := '0';
-
-signal actual_string : string(8 downto 1);
-signal actual_dots : std_logic_vector(8 downto 1);
-
-signal protocol_sel : integer range 0 to 99;
-signal speed_integer : integer range 0 to 99;
-signal speed_exponent : integer range 0 to 9;
-
-
-signal usart_out : std_logic;
-signal usart_out_clock_SIG : std_logic;
-signal usart_TC : std_logic;
-signal uart_prescaler_reset : std_logic;
-signal clock_prescaled_TX : std_logic;
-
-signal fast_TX : std_logic;
-signal fast_RX : std_logic;
-signal uart_trigger : std_logic;
 begin
 	
-	display_inst : entity work.displayController
-		port map(buttonMiddle                => buttonMiddle,
-			     buttonLeft                  => buttonLeft,
-			     buttonRight                 => buttonRight,
-			     buttonUp                    => buttonUp,
-			     buttonDown                  => buttonDown,
-			     clock_dot                   => clock_10Hz,
-			     clock_keyboard              => clock_1kHz,
-			     protocol_sel_out            => protocol_sel,
-			     sci_controller_integer_out  => speed_integer,
-			     sci_controller_exponent_out => speed_exponent,
-			     display_string 			 => actual_string,
-			     display_dots				 => actual_dots);
+--------------- TRANSMITTER -----------------------------------
 	
-	out_clock <= clock_prescaled;
-
-
-	--------------- CLOCK GENERATION ------------------------
-
-	usart_out_clock <= clock_prescaled_TX;
+	Tx_inst : entity work.Tx_MAIN
+		port map(digits       => digits,
+			     segments     => segments,
+			     buttonMiddle => buttonMiddle,
+			     buttonLeft   => buttonLeft,
+			     buttonRight  => buttonRight,
+			     buttonUp     => buttonUp,
+			     buttonDown   => buttonDown,
+			     switchesRaw  => switchesRaw,
+			     LED          => LED,
+			     clock_100MHz => clock_100MHz,
+			     clock_1MHz   => clock_1MHz,
+			     clock_1kHz   => clock_1kHz,
+			     clock_10Hz   => clock_10Hz,
+			     clock_1Hz    => clock_1Hz,
+			     output_data  => usart_out_data,
+			     output_clock => usart_out_clock,
+			     output3      => out_clock);
 	
-	clock_gen_TX_inst : entity work.clockController
-		port map(speed_integer => speed_integer,
-			     speed_exp     => speed_exponent,
-			     clock_100MHz  => clock_100MHz,
-			     clock_out     => clock_prescaled_TX,
-			     reset_presc   => '0',
-			     fast_clock    => fast_TX);
-
-	clock_gen_RX_inst : entity work.clockController
-		port map(speed_integer => speed_integer,
-			     speed_exp     => speed_exponent,
-			     clock_100MHz  => clock_100MHz,
-			     clock_out     => clock_prescaled,
-			     reset_presc   => uart_prescaler_reset,
-			     fast_clock    => fast_RX);
-
-
-
-	--------------- UART ------------------------------------
-
-	uart_inst : entity work.UART_Tx	
-		port map(TxPin    => uart_out,
-			     TxClock  => clock_prescaled_TX,
-			     Data     => switchesRaw(7 downto 0),--uart_data,
-			     DataFlag => uart_trigger,
-			     TC       => uart_TC);
-
-	clock_trig : entity work.prescaler
-		generic map(max_presc => 21)
-		port map(clk_input  => clock_prescaled_TX,
-			     clk_output => uart_trigger,
-			     reset      => '0',
-			     presc      => 20);
-			     
-	uart_recv : entity work.UART_Rx
-		port map(RxPin                => usart_in_data,
-			     fast_clock           => fast_TX,
-			     sampling_clock       => clock_prescaled,
-			     sampling_clock_reset => uart_prescaler_reset,
-			     Data                 => LED(7 downto 0),
-			     DataFlag             => LED(8),
-			     TransmissionError    => LED(9));
-	
-	--------------- USART ------------------------------------
-	
---	usartTX_inst : entity work.USART_Tx
---		port map(TxPin      => usart_out,
---			     TxSynchPin => usart_out_clock_SIG,
---			     TxClock    => clock_prescaled,
---			     Data       => uart_data,
---			     DataFlag   => clock_10Hz,
---			     TC         => usart_TC); 
---	
---	usart_out_clock <= usart_out_clock_SIG;
---	usart_out_data <= usart_out;
---	
---	
---	usartRX_inst : entity work.USART_Rx
---		port map(RxPin             => usart_in_data,
---			     RxSynchPin        => usart_in_clock,
---			     Data              => LED(7 downto 0),
---			     DataFlag          => LED(8),
---			     TransmissionError => LED(9));
-		
 --------------- DEBOUNCING ------------------------------------
 			     
 	debouncerButtonMiddle : entity work.debouncer
